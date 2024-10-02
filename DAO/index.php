9ff919c1e08cc2cb4de3929c05f2a523c65e7913
EnzoLsena <enzolevysena@gmail.com>
2024-10-02 16:41:49 -0300

    classe nova

--- DAO/index.php
+++ DAO/index.php
@@ -1,8 +1,15 @@
 <?php 
 require_once("config.php");
 
-$sql = new Sql();
+$root =new Usuario();
+
+$root->loadById(1);
+echo $root;
+
+
+
+/*$sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
 ?>
