5d021cf70b2288fd29f23af24b9ab168326c0dc0
EnzoLsena <enzolevysena@gmail.com>
2024-10-04 15:45:18 -0300

    atualizando 04/10

---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'enzosena/composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '28f6fd2d4e30357d14f9c8a0b0e9de22163d60b3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'enzosena/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '28f6fd2d4e30357d14f9c8a0b0e9de22163d60b3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
